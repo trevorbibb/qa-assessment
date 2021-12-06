12ea32616640b7467144660d6ef827a967044d0f
Trevor Bibb <75693607+trevorbibb@users.noreply.github.com>
2021-12-06 14:26:33 -0500

    doc: Add comments for potential suggestions

--- cypress/codeReview.js
+++ cypress/codeReview.js
@@ -3,19 +3,29 @@
 //Review the following code sample as if you were giving a peer code review. What feedback/suggestions would you give? 
 
 
+/*** Potential feedback
+ * Nondescriptive describe and test statements (future ease of troubleshooting and reporting value)
+ * Not using variables for elements (code re-usability) 
+ * Long chaining together actions and assertions instead of breaking into new lines or refactoring (code readibility) 
+ * Refactor dragging into own function instead of copy paste 
+ * Copy-paste error in test b would cause it to fail ((cy.get('#column-b')) should be column-a)
+ */ 
+
+
 import cy from 'cypress'
 
 describe('Open the page', () => {
-    it('opens url', () => {
-        cy.visit('http://the-internet.herokuapp.com/drag_and_drop')
+    it('opens url', () => { //not descriptive for setup 
+        cy.visit('http://the-internet.herokuapp.com/drag_and_drop')  
     }); 
 
     it('tests a', () => { 
-        cy.get('#column-a').trigger('dragStart', new DataTransfer).cy.get('#column-b').trigger('drop',{DataTransfer})
+        cy.get('#column-a').trigger('dragStart').cy.get('#column-b').trigger('drop')
         cy.get('#columns').children().first().should.be(cy.get('#column-b'))
     }); 
     it('tests b', () => { 
-        cy.get('#column-b').trigger('dragStart', new DataTransfer).cy.get('#column-a').trigger('drop',{DataTransfer})
-        cy.get('#columns').children().first().should.be(cy.get('#column-a'))
+        cy.get('#column-b').trigger('dragStart').cy.get('#column-a').trigger('drop')
+        cy.get('#columns').children().first().should.be(cy.get('#column-b'))
     })
 }) 
+
